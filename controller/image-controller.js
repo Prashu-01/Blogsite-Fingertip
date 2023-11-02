79fe9f1f24ccc4a9ae20df3e9bfd5b053cbcd2a4
Prashu Verma <prashuverma012@gmail.com>
2023-11-02 23:08:16 +0530

    imglink

--- controller/image-controller.js
+++ controller/image-controller.js
@@ -1,8 +1,8 @@
-// import { request, response } from "express"
+
 import grid from 'gridfs-stream'
 import mongoose from 'mongoose'
 
-const url='http://localhost:8000'
+const url='https://famous-tick-lingerie.cyclic.app/'
 
 let gfs,gridfsBucket 
 const conn=mongoose.connection
